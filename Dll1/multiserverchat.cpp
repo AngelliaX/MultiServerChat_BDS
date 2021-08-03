262df08f063848b1eeca2a2a28bdcc0af1dac305
TungstenVn <huutung2003@gmail.com>
2021-08-03 12:59:42 +0700

    up

--- Dll1/multiserverchat.cpp
+++ Dll1/multiserverchat.cpp
@@ -16,35 +16,39 @@ void connectDB() {
     }
     else { puts("Connection to database has failed!"); }
 }
-#include <api/scheduler/scheduler.h>
-void schTask() {
-    Handler::schedule(RepeatingTask([] {
-        int temp_state = mysql_query(conn, "SELECT * FROM sur");
-        if (!temp_state)
+//#include <api/scheduler/scheduler.h>
+
+THook(void, "?tick@Level@@UEAAXXZ", class Level* lv) {
+    original(lv);//Don't remove this line
+    int temp_state = mysql_query(conn, "SELECT * FROM sur");
+    if (!temp_state)
+    {
+        res = mysql_store_result(conn);
+        while (row = mysql_fetch_row(res))
         {
-            res = mysql_store_result(conn);
-            string color1 = "\xc2\xa7" + std::string("b");
-            string color2 = "\xc2\xa7" + std::string("r");
-            string color3 = "\xc2\xa7" + std::string("a");
-            while (row = mysql_fetch_row(res))
+            //printf("ID: %s, Server: %s, Name: %s, Text: %s\n", row[0], row[1], row[2], row[3]);
+            for (Player* pl : liteloader::getAllPlayers())
             {
-                //printf("ID: %s, Server: %s, Name: %s, Text: %s\n", row[0], row[1], row[2], row[3]);
-                for (Player* pl : liteloader::getAllPlayers())
-                {
-                    WPlayer player = WPlayer{ *pl };
-                    player.sendText("<"+color1+std::string(row[1])+color2+"> <"+color3+ std::string(row[2]) +color2+"> "+ std::string(row[3]), TextType::RAW);
-                }
-                string text = "DELETE FROM sur WHERE id = " + std::string(row[0]);
-                temp_state = mysql_query(conn, text.c_str());
-                if (temp_state)
-                {
-                    puts("Can't delete data in table");
-                }
+                WPlayer player = WPlayer{ *pl };
+                std::string txt = row[3];
+                player.sendText(txt, TextType::RAW);
+            }
+            string text = "DELETE FROM sur WHERE id = " + std::string(row[0]);
+            temp_state = mysql_query(conn, text.c_str());
+            if (temp_state)
+            {
+                puts("Can't delete data in table");
             }
         }
-        else { std::cout << "Query failed: " << mysql_error(conn) << std::endl; }
-    }, 1)); //0.5sec
+    }
+    else { std::cout << "Query failed: " << mysql_error(conn) << std::endl; }
 }
+
+//void schTask() {
+//    Handler::schedule(RepeatingTask([] {
+//        
+//    }, 1)); //0.5sec
+//}
 
 
 bool playerChatEvent(ChatEV e) {
@@ -54,20 +58,20 @@ bool playerChatEvent(ChatEV e) {
     {
         puts("Can't insert into table");
     }
-    for (Player* pl : liteloader::getAllPlayers())
+   /* for (Player* pl : liteloader::getAllPlayers())
     {
         WPlayer player = WPlayer{ *pl };
         string color1 = "\xc2\xa7"+std::string("a");
         string color2 = "\xc2\xa7" + std::string("r");
         player.sendText("<"+ color1 + std::string(e.pl->getNameTag()) + color2 +"> " + std::string(e.msg), TextType::RAW);
-    }
+    }*/
     return false;
 }
 
 void multiserverchat_entry(){
 	Event::addEventListener(playerChatEvent);
     connectDB();
-    schTask();
+    //schTask();
     puts("\n                  _ _   _               _           _   \n                 | | | (_)             | |         | |  \n  _ __ ___  _   _| | |_ _ _____   _____| |__   __ _| |_ \n | '_ ` _ \| | | | | __| / __\ \ / / __| '_ \ / _` | __|\n | | | | | | |_| | | |_| \__ \\ V / (__| | | | (_| | |_ \n |_| |_| |_|\__,_|_|\__|_|___/ \_/ \___|_| |_|\__,_|\__|");                                                      
     puts("                               made by tungnguyen\n");
 }
